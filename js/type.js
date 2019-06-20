2f2eb045c15dbfe51ccc546e5bc0df7591ca2c92
chandan sharma <sharmachandan351@gmail.com>
2019-06-20 17:17:33 +0545

    responsive design for mobile devices added

--- js/type.js
+++ js/type.js
@@ -5,8 +5,9 @@ const typing=()=>{
     if(i<heading.length){
         document.querySelector(".typing").innerHTML+=heading.charAt(i);
         i++;
-        
         setTimeout(typing,150);
     }
+
 }
+
 typing();
