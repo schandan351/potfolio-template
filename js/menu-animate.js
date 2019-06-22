1658426c7cc0a06b47a1fd158a50d429d714564b
chandan sharma <sharmachandan351@gmail.com>
2019-06-22 18:15:34 +0545

    mobile menu not close problem solved

--- js/menu-animate.js
+++ js/menu-animate.js
@@ -3,3 +3,7 @@ var mainmenu=document.querySelector(".menu-items");
 document.querySelector(".hamburger").onclick=function(){
     mainmenu.classList.toggle("open");
 }
+
+document.querySelector(".menu-items .close-menu").onclick=function(){
+    mainmenu.classList.remove("open");
+}
